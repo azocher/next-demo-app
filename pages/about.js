8a1317b485f7830a1f73ab0a4e581c8daa4bdd34
Anna Zocher <anna.zocher@gmail.com>
2021-03-04 11:57:19 -0800

    updated about page

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,7 @@ export default function About() {
     return (
         <div>
             <h1>Hi - my name is Anna! 🐳</h1>
+            <h2>extra info about me!</h2>
             <Image src="/jpod.jpg" alt="my profile pic" width={125} height={125} />
         </div>
     )
